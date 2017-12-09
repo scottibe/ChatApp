06da69f1ead6776fb8d59d09163109c6d2721df2
Scott Bewick <bewick.scott@gmail.com>
2017-12-09 15:55:32 -0600

    first commit

--- db/migrate/20171209183026_create_conversations.rb
+++ db/migrate/20171209183026_create_conversations.rb
@@ -6,7 +6,8 @@ class CreateConversations < ActiveRecord::Migration[5.1]
 
       t.timestamps
     end
-    add_index :conversations, :recipient_id
-    add_index :conversations, :sender_id
+    
+    add_index :conversations, [:recipient_id, :sender_id], unique: true
+  
   end
 end
